d8c1d6b861168e8aae09b1e1780e08280709a4b0
arkango <asaracino@aryma.it>
2018-03-23 15:38:33 +0100

    creazione anagrafica

--- fl_modules/mod_anagrafica2/mod_inserisci.php
+++ fl_modules/mod_anagrafica2/mod_inserisci.php
@@ -1,24 +1,24 @@
-<?php 
+<?php
 
-require_once('../../fl_core/autentication.php');
+require_once '../../fl_core/autentication.php';
 $loadSelectComuni = 1;
-$id = ($_SESSION['usertype'] > 1) ? $_SESSION['anagrafica'] : check($_GET['id']);
-if($_SESSION['usertype'] > 1) $force_id = $_SESSION['anagrafica']; 
 
-if($id > 1) {
-$profilo = @GRD('fl_anagrafica',@$id); 
-//$user_photo = "../../../set/img/photo_cv/".$profilo['id'].".jpg";	
-//$user_photo = (@!file_exists($user_photo)) ? '<p style="font-size: 300%; padding: 20px; color: #CACACC"><i class="fa fa-smile-o"></i></p>' : '<p class="user_photo"><span class="user_corda"><img  data-file="'.$user_photo.'" src="'.$user_photo.'" alt="Img" /></span></p>';
+$id = ($_SESSION['usertype'] > 1) ? $_SESSION['anagrafica'] : check($_GET['id']);
+
+if ($id > 1) {
+    $profilo = @GRD('fl_anagrafica', @$id);
 }
 
+include 'fl_settings.php'; // Variabili Modulo
 
-include('fl_settings.php'); // Variabili Modulo 
+include "../../fl_inc/headers.php";
 
-include("../../fl_inc/headers.php");
-if(!isset($_GET['view'])) include("../../fl_inc/testata_mobile.php");
+if (!isset($_GET['view'])) {
+    include "../../fl_inc/testata_mobile.php";
+}
 
-$tab_div_labels = array('marchio' => 'Profilo',  'tipo_documento' => "Dati Documento", 'forma_giuridica' => "Dati Fiscali", 'tipologia_attivita' => $etichette_anagrafica['tipologia_attivita'], 'telefono' => "Contatti", 'note' => "Note");
- ?>
+$tab_div_labels = array('marchio' => 'Profilo', 'tipo_documento' => "Dati Documento", 'forma_giuridica' => "Dati Fiscali", 'tipologia_attivita' => $etichette_anagrafica['tipologia_attivita'], 'telefono' => "Contatti", 'note' => "Note");
+?>
 
 
 
@@ -33,12 +33,15 @@ $tab_div_labels = array('marchio' => 'Profilo',  'tipo_documento' => "Dati Docum
 <div id="content_scheda">
 
 <div class="info_dati">
-<?php if($id > 1) { 
-$telefono = phone_format($profilo['telefono'],'39');
-echo '<h1><strong>'.$profilo['ragione_sociale'].'</strong> ('.$profilo['nome'].' '.$profilo['cognome'].')</h1>';
-if(ALERT_DOCUMENTO_SCADUTO == 1)  echo '<h2>Tipo Delega: <span class="msg gray">'.@$pagamenti_f24[@$profilo['pagamenti_f24']].'</span></h2>';
-echo '<p>Telefono: <a href="tel:'.@$telefono.'">'.@$telefono.'</a> mail: <a href="mailto:'.@$profilo['email'].'" >'.@$profilo['email'].'</a></h2>';
-} else { echo '<h1>Nuovo '.$tipo_profilo[ $tipo_profilo_id].'</h1>'; }
+<?php if ($id > 1) {
+    $telefono = phone_format($profilo['telefono'], '39');
+    echo '<h1><strong>' . $profilo['ragione_sociale'] . '</strong> (' . @$profilo['nome'] . ' ' . @$profilo['cognome'] . ')</h1>';
+    if (ALERT_DOCUMENTO_SCADUTO == 1) {
+        echo '<h2>Tipo Delega: <span class="msg gray">' . @$pagamenti_f24[@$profilo['pagamenti_f24']] . '</span></h2>';
+    }
+
+    echo '<p>Telefono: <a href="tel:' . @$telefono . '">' . @$telefono . '</a> mail: <a href="mailto:' . @$profilo['email'] . '" >' . @$profilo['email'] . '</a></h2>';
+} else {echo '<h1>Nuovo ' . $tipo_profilo[$tipo_profilo_id] . '</h1>';}
 
 ?>
 
@@ -47,21 +50,62 @@ echo '<p>Telefono: <a href="tel:'.@$telefono.'">'.@$telefono.'</a> mail: <a href
 
 </div>
 
-<?php if(isset($_GET['esito'])) { $class = (isset($_GET['success'])) ? 'green' : 'red'; echo '<p class="esito '.$class.'">'.check($_GET['esito']).'</p>'; }  ?>
+<?php if (isset($_GET['esito'])) {$class = (isset($_GET['success'])) ? 'green' : 'red';
+    echo '<p class="esito ' . $class . '">' . check($_GET['esito']) . '</p>';}?>
 
 <div id="map-canvas"></div>
 <form id="scheda" action="../mod_basic/action_modifica.php" method="post" enctype="multipart/form-data">
 
 
 
-<?php include('../mod_basic/action_estrai.php');  ?>
+<?php include '../mod_basic/action_estrai.php';?>
 <input type="hidden" name="info" value="1" />
 
 <input type="hidden" name="dir_upfile" value="icone_articoli" />
 
-<!--<input type="hidden" name="goto" value="../mod_account/mod_inserisci.php?id=1" />-->
-
+<?php 
+if(!isset($_GET['error']) && isset($_GET['first']) && $_GET['id'] > 1){
+    echo '<input type="hidden" name="goto" value="../mod_account/mod_inserisci.php?id=1&&anagrafica_id='.$id.'" />';
+}
+?>
 </form>
+
+<script>
+
+$('#invio').attr('id','nosend');
+$('#nosend').click(function(){
+    event.preventDefault();
+
+        //validate fields
+        var fail = false;
+        var fail_log = '';
+        $( '#scheda' ).find( 'select, textarea, input' ).each(function(){
+            if( ! $( this ).prop( 'required' )){
+
+            } else {
+                if ( ! $( this ).val() ) {
+                    fail = true;
+                    name = $( this ).attr( 'name' );
+                    fail_log += name + " is required \n";
+                }
+
+            }
+        });
+
+
+        //submit if fail never got set to true
+        if ( ! fail ) {
+
+        $('#scheda').submit();
+
+        } else {
+
+            alert( fail_log );
+
+        }
+});
+
+</script>
 
 
 </div></div></body></html>

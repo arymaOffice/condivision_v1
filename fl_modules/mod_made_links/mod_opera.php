12f11572151c66dbbfec59409b82225eef37a25b
arkango <asaracino@aryma.it>
2018-03-30 14:17:00 +0200

    statistiche e servizi imporved

--- fl_modules/mod_made_links/mod_opera.php
+++ fl_modules/mod_made_links/mod_opera.php
@@ -27,7 +27,7 @@ if ($_GET['form']) {
     if (isset($params['n_monitor']) && isset($params['pollici'])) {
         $params['n_monitor'] = check($params['n_monitor']);
         //inserimento per statistiche numero monitor
-        mysql_query('INSERT INTO fl_data_link_mon (id_link,id_n_monitor,id_account) VALUES (' . $id . ',' . $params['n_monitor'] . ',' . $_SESSION['number'] . ')', CONNECT);
+        mysql_query('INSERT INTO fl_data_link_mon (id_link,n_monitor,id_account) VALUES (' . $id . ',' . $params['n_monitor'] . ',' . $_SESSION['number'] . ')', CONNECT);
 
         for ($i = 1; $i <= $params['n_monitor']; $i++) {
             array_push($links['data'], 'http://service.1x2live.it/index' . $external . '.html?id=' . $link_id . '&monitor_id=' . $i . '&monitor_count=' . $params['n_monitor'] . '&token=' . $token);
@@ -38,7 +38,7 @@ if ($_GET['form']) {
     } elseif (isset($params['pollici'])) {
 
         //inserimento per statistiche numero monitor
-        mysql_query('INSERT INTO fl_data_link_mon (id_link,id_n_monitor,id_account) VALUES (' . $id . ',1,' . $_SESSION['number'] . ')', CONNECT);
+        mysql_query('INSERT INTO fl_data_link_mon (id_link,n_monitor,id_account) VALUES (' . $id . ',1,' . $_SESSION['number'] . ')', CONNECT);
 
         array_push($links['data'], 'http://service.1x2live.it/index' . $external . '.html?id=' . $link_id . '&monitor_id=1&monitor_count=1' . '&token=' . $token);
         $links['esito'] = 1;
---
+++ fl_modules/mod_statistiche/README.md
@@ -0,0 +1,10 @@
+modulo riservato alle statistiche raccolte
+con l'aiuto degli utenti
+
+risoluzione scelta 
+
+la media del numero dei monito scelti
+
+evento più visto
+
+andamneto nell'anno 
